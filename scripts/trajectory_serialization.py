c5e15b08be323acfc11b10d3ec570021eacfb07e
Arne Peters <arne.peters@tum.de>
2018-02-15 13:48:54 +0100

    supporting package:// to save to specific ROS package directories

--- scripts/trajectory_serialization.py
+++ scripts/trajectory_serialization.py
@@ -2,19 +2,22 @@
 
 from moveit_cartesian_plan_plugin.srv import *
 import rospy
+import rospkg
 import os
 import os.path
+import string
 from rospy_message_converter import message_converter
 from rospy_message_converter import json_message_converter
 from moveit_msgs.msg import RobotTrajectory
 
 def handle_export_trajectory(req):
-    rospy.loginfo("Exporting trajectory to %s", req.file_name)
+    file_name = resolveFileName(req.file_name)
+    rospy.loginfo("Exporting trajectory to %s", file_name)
 
-    if os.path.exists(req.file_name):
+    if os.path.exists(file_name):
         # file exists
 
-        if os.path.isdir(req.file_name):
+        if os.path.isdir(file_name):
             rospy.logerr("Target file is a directory.")
             return ExportTrajectoryResponse(success=False, error="Target file is a directory.")
         elif not (req.overwrite):
@@ -22,26 +25,28 @@ def handle_export_trajectory(req):
             return ExportTrajectoryResponse(success=False, error="Target file alreay exists.")
 
         rospy.logwarn("Target file alreay exists - Overwriting...")
-        os.remove(req.file_name)
+        os.remove(file_name)
 
-    file = open(req.file_name, "w")
+    file = open(file_name, "w")
     file.write(json_message_converter.convert_ros_message_to_json(req.trajectory))
     file.close()
     return ExportTrajectoryResponse(success=True)
 
 def handle_import_trajectory(req):
-    rospy.loginfo("Loading trajectory from %s", req.file_name)
+    file_name = resolveFileName(req.file_name)
 
-    if not os.path.exists(req.file_name):
+    rospy.loginfo("Loading trajectory from %s", file_name)
+
+    if not os.path.exists(file_name):
         rospy.logerr("File not found.")
         return ImportTrajectoryResponse(success=False, error="File not found.")
 
-    if os.path.isdir(req.file_name):
+    if os.path.isdir(file_name):
         rospy.logerr("Target file is a directory.")
         return ImportTrajectoryResponse(success=False, error="Target file is a directory.")
 
 
-    file = open(req.file_name, "r")
+    file = open(file_name, "r")
     content = file.read()
     file.close()
 
@@ -50,11 +55,23 @@ def handle_import_trajectory(req):
 
     return ImportTrajectoryResponse(success=True, trajectory=trajectory)
 
+def resolveFileName(file_name):
+    copy = file_name.split("://", 1)
+
+    if len(copy) > 1 and copy[0] == "package":
+        copy = copy[1].split("/", 1)
+        rospack = rospkg.RosPack()
+        return rospack.get_path(copy[0])+"/"+copy[1]
+
+    return file_name
+
+
 def trajectory_serialization_server():
     rospy.init_node('trajectory_serialization_server')
     exportService = rospy.Service('export_trajectory', ExportTrajectory, handle_export_trajectory)
     importService = rospy.Service('import_trajectory', ImportTrajectory, handle_import_trajectory)
     print "Trajectory Serialization ready."
+
     rospy.spin()
 
 if __name__ == "__main__":
